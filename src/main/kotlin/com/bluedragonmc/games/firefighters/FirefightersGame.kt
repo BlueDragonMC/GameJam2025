871f912a311f5a711667fd2f35169379d22dfc3d
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-07-18 23:45:52 -0400

    Fix item dropping before game starts

--- src/main/kotlin/com/bluedragonmc/games/firefighters/FirefightersGame.kt
+++ src/main/kotlin/com/bluedragonmc/games/firefighters/FirefightersGame.kt
@@ -10,6 +10,7 @@ import com.bluedragonmc.server.event.GameStartEvent
 import com.bluedragonmc.server.module.combat.CustomDeathMessageModule
 import com.bluedragonmc.server.module.combat.OldCombatModule
 import com.bluedragonmc.server.module.config.ConfigModule
+import com.bluedragonmc.server.module.gameplay.InventoryPermissionsModule
 import com.bluedragonmc.server.module.gameplay.SidebarModule
 import com.bluedragonmc.server.module.instance.InstanceContainerModule
 import com.bluedragonmc.server.module.instance.InstanceTimeModule
@@ -41,6 +42,7 @@ import net.minestom.server.entity.metadata.display.BlockDisplayMeta
 import net.minestom.server.event.EventListener
 import net.minestom.server.event.entity.EntityAttackEvent
 import net.minestom.server.event.instance.InstanceTickEvent
+import net.minestom.server.event.item.ItemDropEvent
 import net.minestom.server.event.player.PlayerDeathEvent
 import net.minestom.server.event.player.PlayerTickEvent
 import net.minestom.server.event.player.PlayerUseItemEvent
@@ -179,7 +181,8 @@ class FirefightersGame(mapName: String) : Game(GAME_NAME, mapName) {
                 parent.handleEvent(EventListener.builder(InstanceTickEvent::class.java).handler {
                     if (System.currentTimeMillis() > parent.explodingUntil) { // Wait until previous cutscene finishes playing
                         playedRevealCutscene = true
-                        val points = parent.getModule<ConfigModule>().getConfig().node("stage2RevealCutscene")?.getList(Pos::class.java)
+                        val points = parent.getModule<ConfigModule>().getConfig().node("stage2RevealCutscene")
+                            ?.getList(Pos::class.java)
                         if (points != null && !points.isEmpty()) {
                             for (player in parent.players) {
                                 parent.getModule<CutsceneModule>().playCutscene(
@@ -277,7 +280,8 @@ class FirefightersGame(mapName: String) : Game(GAME_NAME, mapName) {
                 parent.handleEvent(EventListener.builder(InstanceTickEvent::class.java).handler {
                     if (System.currentTimeMillis() > parent.explodingUntil) { // Wait until previous cutscene finishes playing
                         playedVillageCutscene = true
-                        val points = parent.getModule<ConfigModule>().getConfig().node("stage3RevealCutscene")?.getList(Pos::class.java)
+                        val points = parent.getModule<ConfigModule>().getConfig().node("stage3RevealCutscene")
+                            ?.getList(Pos::class.java)
                         if (points != null && !points.isEmpty()) {
                             for (player in parent.players) {
                                 parent.getModule<CutsceneModule>().playCutscene(
@@ -386,6 +390,7 @@ class FirefightersGame(mapName: String) : Game(GAME_NAME, mapName) {
 //        use(VoteStartModule(minPlayers = 1, countdownSeconds = 5))
         use(WinModule())
         use(CustomDeathMessageModule())
+        use(InventoryPermissionsModule(allowDropItem = false, allowMoveItem = false), { _ -> state != GameState.INGAME })
 
         handleEvent<PlayerUseItemEvent> { event ->
             // TODO - why does this make it work?? why doesn't the module receive the event on its own???????
@@ -459,7 +464,7 @@ class FirefightersGame(mapName: String) : Game(GAME_NAME, mapName) {
                         cutsceneModule.playCutscene(
                             player,
                             this,
-                            arsonistCutscene, 
+                            arsonistCutscene,
                             stepsPerCurve = 10,
                             msPerPoint = 300
                         )
@@ -584,7 +589,7 @@ class FirefightersGame(mapName: String) : Game(GAME_NAME, mapName) {
 
         // Poison Damage
 
-        eventNode.addListener (PlayerTickEvent::class.java) { event ->
+        eventNode.addListener(PlayerTickEvent::class.java) { event ->
             val player = event.player
             if (player.hasEffect(PotionEffect.POISON)) {
                 if ((player.aliveTicks % 20).toInt() == 0) {
@@ -634,7 +639,8 @@ class FirefightersGame(mapName: String) : Game(GAME_NAME, mapName) {
             max(pos1.blockY(), pos2.blockY()),
             max(pos1.blockZ(), pos2.blockZ())
         )
-        val playersToKill = players.filter { player -> player.position.x in (minimum.x..maximum.x) && player.position.y in (minimum.y..maximum.y) && player.position.z in (minimum.z..maximum.z) }
+        val playersToKill =
+            players.filter { player -> player.position.x in (minimum.x..maximum.x) && player.position.y in (minimum.y..maximum.y) && player.position.z in (minimum.z..maximum.z) }
 
         explodingUntil = System.currentTimeMillis() + msPerPoint * stepsPerCurve * points.size
 
